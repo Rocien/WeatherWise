66d48ac5908d230f3fbeec24675f4b512533c097
nkun0003 <nkun0003@algonquinlive.com>
2024-12-02 22:17:54 -0500

    Initial Commit

---
+++ WeatherWise/WeatherWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWiseApp.swift
+//  WeatherWise
+//
+//  Created by Rocien Nkunga on 02/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
